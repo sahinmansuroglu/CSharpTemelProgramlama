29827b272f14507475842a07d25a3604ca9c3fcc
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2020-12-15 23:24:41 +0300

    Add files via upload

---
+++ 5.7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cEgitim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mesaj = "13 Ekim 1923'te TBMM'de ANKARA başkent olarak kabul edilmiştir";
+            string kelime1 = "  Ankara   ";
+            string kelime2 = "  BAŞKENT   ";
+
+            kelime1 = kelime1.Trim();
+            kelime1 = kelime1.ToUpper();
+            kelime2 = kelime2.Trim();
+            kelime2 = kelime2.ToLower();
+
+            Console.WriteLine(mesaj.Contains(kelime1));
+            Console.WriteLine(mesaj.Contains(kelime2));
+        }
+    }
+}
